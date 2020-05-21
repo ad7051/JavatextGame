a0db8310f90e8aa6b600b78b91d99f8223b2b647
ad7051 <63580888+ad7051@users.noreply.github.com>
2020-05-21 20:47:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 //자바에서 c로 이식할 예쩡
 int main(){
-return 0;
+  int menu;
+  printf("Hello, Welcome to TRPG-Ver.C.1\n");
+  menu=MainGame();
+  switch(menu){
+    case 1: printf("GameOver\n");
+      break;
+    case 2: printf("You Win\nThank You For Play\n");
+      break;
+    case 3: printf("See You Next Time\n");
+      break;
+    default: printf("What happened?\n");
+  }
+  printf("Program is end");
+  return 0;
 }
